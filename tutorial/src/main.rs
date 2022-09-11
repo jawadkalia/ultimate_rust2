1c57a3680b4bd88e4bed2b64fda0687ead637d67
AhyaanTech <ahyaantech@gmail.com>
2022-09-11 02:11:52 -0500

    finishing tutorial

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,46 @@
+use rusty_engine::prelude::*;
+
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    // Setup game here
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = SOUTH_WEST;
+    player.layer = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
+
+    game.add_logic(game_logic);
+    game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    for event in engine.collision_events.drain(..) {
+        println!("{:?}", event)
+    }
+
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
 }
